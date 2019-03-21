d165977579846491666a1ca69f6e0d50cde6c819
cesareduardo <cesareduardo824@gmail.com>
2019-03-21 04:05:51 -0500

    Muestra teatro en fiura

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -52,7 +52,8 @@ var cine = (function () {
                     var funcion = $(tr).find('td:nth-child(2)').text();
                     var canntidadSillas = $(tr).find('td:nth-child(3)').text();
                     var fecha = $(tr).find('td:nth-child(4)').text();
-                    console.log('funcion: ' + funcion + ' - cantidad Sillas: ' + canntidadSillas + ' fecha: ' + fecha );
+                    console.log('funcion: ' + funcion + ' - cantidad Sillas: ' + canntidadSillas + ' fecha: ' + fecha +'las sillas'+sillasNum);
+                    mostrarSillas();
                 }
             })
         })
@@ -63,9 +64,37 @@ var cine = (function () {
     
     function mostrarSillas(){
         
+        var markup="";
+        $("table ").clearQueue();
+        $(document.sillas).ready(function(){
+        
+            for(var y=0; y<7;y++){
+                var temp=sillasNum[y];
+                markup+="<tr>";
+                for(var u=0;u<12;u++){
+                    var tem=temp[u];
+                   // alert("miremos valor "+tem);
+                    markup+="<br>";
+                    if(tem==true){
+                        
+                        markup+="<br>";
+                        markup+="<td bgcolor='#00FF00'></td>";                        
+                        markup+="<br>";
+                    }else{
+                        //alert("salimos");
+                        
+                        markup+="<td bgcolor='#FF0000'></td>";
+                        
+                    }   
+                }
+                markup+="</tr>";
+            }
+            markup+="</tr>";
+            $("table ").append(markup);
+            
         
         
-        
+    });    
         
     }
     
@@ -76,7 +105,7 @@ var cine = (function () {
             for (var i = 0; i < t.length; i++)
             {
                 fullname += [t[i].movie.name, t[i].seats.length * 12, t[i].date].join(" ");
-                $(document).ready(function () {
+                $(document.tabs).ready(function () {
                     var name = t[i].movie.name;
                     sillasNum=t[i].seats;
                     var puestos = t[i].seats.length * 12;
