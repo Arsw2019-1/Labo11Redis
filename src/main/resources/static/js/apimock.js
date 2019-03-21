00b2e6eb0334f83d7b791117d1fa2e49d8fa0736
cesareduardo <cesareduardo824@gmail.com>
2019-03-21 00:41:55 -0500

    Avance mostrando cines

--- src/main/resources/static/js/apimock.js
+++ src/main/resources/static/js/apimock.js
@@ -1,30 +1,48 @@
 //@author dsaavedra
+var elec = "";
+        var apimock = (function(){
 
-var apimock = (function(){
-
-var mockdata = [];
-        var mockdata=new Array();
-        mockdata.push({"name":"Cine80", "functions":[{"movie":{"name":"Titanic Movie", "genre":"Action"}, "seats":[[true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]], "date":"2018-12-18 15:30"}, {"movie":{"name":"The Purge", "genre":"Horror"}, "seats":[[true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]], "date":"2018-12-18 15:30"}]});
-        mockdata.push({"name":"Cine112", "functions":[{"movie":{"name":"mision imposible", "genre":"Fiction"}, "seats":[[true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]], "date":"2018-12-18 15:31"}]});
-        var names = new Array();
-        mockdata["Cine80"] = [{"name":"Cine80", "functions":[{"movie":{"name":"Titanic Movie", "genre":"Action"}, "seats":[[true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]], "date":"2018-12-18 15:30"}, {"movie":{"name":"The Purge", "genre":"Horror"}, "seats":[[true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]], "date":"2018-12-18 15:30"}]}];
-        mockdata["Cine112"] = [{"name":"Cine112", "functions":[{"movie":{"name":"mision imposible", "genre":"Fiction"}, "seats":[[true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]], "date":"2018-12-18 15:31"}]}];
-
+        var mockdata = [];
+                var mockdata = new Array();
+                mockdata.push({name:"Cine80", functions:[{movie:{"name":"Titanic Movie", "genre":"Action"}, seats:[[true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]], date:"2018-12-18 15:30"}, {movie:{name:"The Purge", genre:"Horror"}, seats:[[true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]], date:"2018-12-18 15:30"}]});
+                mockdata.push({name:"Cine112", functions:[{movie:{"name":"mision imposible", "genre":"Fiction"}, seats:[[true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]], date:"2018-12-18 15:31"}]});
+                var names = new Array();
+                mockdata["Cine80"] = [{name:"Cine80", functions:[{movie:{"name":"Titanic Movie", "genre":"Action"}, seats:[[true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]], date:"2018-12-18 15:30"}, {movie:{name:"The Purge", genre:"Horror"}, seats:[[true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]], date:"2018-12-18 15:30"}]}];
+                mockdata["Cine112"] = [{name:"Cine112", functions:[{movie:{"name":"mision imposible", "genre":"Fiction"}, seats:[[true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]], date:"2018-12-18 15:31"}]}];
                 for (var i = 0; i < mockdata.length; i++){
-                        names.push(mockdata[i].name);
-                }    
-    
-        return {
-                getCinemaByName:function(name, callback){
-                    callback(
-                        mockdata[name]
-                    );
-                 },
-                getByNamesCinemas:function(){
-
- 
-                        return names;
-
-                }
+        names.push(mockdata[i].name);
         }
-})();
+        return {
+        getCinemaByName:function(name, callback){
+        callback(
+           
+                
+                mockdata[name]
+                );
+        },
+                getByNamesCinemas:function(){
+                return names;
+                },
+                getByNamesCine:function(name){
+                    var resp="";
+                    for(var i = 0; i < mockdata.length; i++){
+                        if(mockdata[i].name==name){
+                            alert("urra"+mockdata[i].name);
+                            resp=mockdata[i];
+                            alert(resp.name);
+                            break;
+                            
+                            
+                        }
+                        
+                        
+                    }
+                    
+                return resp;
+                }
+
+
+
+
+        }
+        })();
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,35 +1,61 @@
-
-
 $("#connect").on("click", cine);
-var cinemas ="";
+
+var persons = [];
+var cinemas = "";
 var cine = (function () {
 
     function getAllCinemas() {
 
         cinemas = apimock.getByNamesCinemas();
-        
+
         $("#listacinemas").append("<tr><td>" + cinemas + "</td></tr>");
 
     }
 
-    function selectCinema(){
-        alert("hola mundo");
-        for(var x=0; x<cinemas.length;x++){
-            alert(cinemas[x]);
-            document.getElementById("cine").innerHTML += ("<option>"+cinemas[x]+"</option>");
-            
+    function selectCinema() {
+        for (var x = 0; x < cinemas.length; x++) {
+            document.getElementById("cine").innerHTML += ("<option>" + cinemas[x] + "</option>");
         }
 
+    }
+    function selec() {
+        var texto = "";
+        texto = "El numero de opciones del select: " + document.form2.cine.length
+        var indice = document.form2.form2.selectedIndex
+        texto += "nIndice de la opcion escogida: " + indice
+        var valor = document.form2.form2.options[indice].value
+        texto += "nValor de la opcion escogida: " + valor
+        alert("putos es este" + valor);
+        var textoEscogido = document.form2.form2.options[indice].text
+        texto += "nTexto de la opcion escogida: " + textoEscogido
+        alert(texto)
 
+
+        myFunction(apimock.getByNamesCine(valor));
 
     }
 
 
+    function getFullName() {
+        var fullname = "";
+        for (var x = 0; x < persons.length; x++) {
+            alert(persons[x].name);
+            alert(persons[x].functions[x].movie.name);
+            alert(persons[x].functions[x].seats.length * 12);
+            alert(persons[x].functions[x].date);
+            fullname = [persons[x].name, persons[x].functions[x].movie.name, persons[x].functions[x].seats.length * 12, persons[x].functions[x].date].join(" ");
+        }
+        return fullname;
+    }
+
+    function myFunction(ter) {
+        persons[0]=ter;
+        document.getElementById("infoCinema").innerHTML = persons.map(getFullName);
+    }
+
     return {
         getAllCinemas: getAllCinemas,
-        selectCinema:selectCinema
-
+        selectCinema: selectCinema,
+        selec: selec
     };
 })();
-
-
